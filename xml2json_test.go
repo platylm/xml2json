d1068dd85a6ef8cbca2c41775912ae764642dd57
platylm <n_nareenart@kkumail.com>
2018-08-16 12:03:33 +0700

    [Edited]:refactor code

--- xml2json_test.go
+++ xml2json_test.go
@@ -6,55 +6,31 @@ import (
 	"testing"
 )
 
-type Envelope struct {
-	xmlName xml.Name `xml:Envelope`
-	Body    Body
-}
-
-type Body struct {
-	xmlName                       xml.Name `xml:Body`
-	GetCountriesAvailableResponse GetCountriesAvailableResponse
-}
-
-type GetCountriesAvailableResponse struct {
-	xmlName                     xml.Name `xml:GetCountriesAvailableResponse`
-	GetCountriesAvailableResult GetCountriesAvailableResult
-}
-
-type GetCountriesAvailableResult struct {
-	xmlName     xml.Name      `xml:GetCountriesAvailableResult`
-	CountryCode []CountryCode `xml:GetCountriesAvailableResult>CountryCode`
+type GetCountriesAvailable struct {
+	CountryCode []CountryCode `xml:"Body>GetCountriesAvailableResponse>GetCountriesAvailableResult>CountryCode"`
 }
 
 type CountryCode struct {
 	Code        string `xml:"Code"`
-	Description string `xml:Description`
+	Description string `xml:"Description"`
 }
 
 func Test_Convert_XML_to_JSON_Should_Be_JSON(t *testing.T) {
-	var actual Envelope
+	var actual GetCountriesAvailable
 	xmlFile, _ := ioutil.ReadFile("./response.xml")
-	expected := Envelope{
-		Body: Body{
-			GetCountriesAvailableResponse: GetCountriesAvailableResponse{
-				GetCountriesAvailableResult: GetCountriesAvailableResult{
-					CountryCode: []CountryCode{
-						CountryCode{"Canada", "Canada"},
-						CountryCode{"GreatBritain", "Great Britain and Wales"},
-						CountryCode{"IrelandNorthern", "Northern Ireland"},
-						CountryCode{"IrelandRepublicOf", "Republic of Ireland"},
-						CountryCode{"Scotland", "Scotland"},
-						CountryCode{"UnitedStates", "United States"},
-					},
-				},
-			},
+	expected := GetCountriesAvailable{
+		CountryCode: []CountryCode{
+			CountryCode{"Canada", "Canada"},
+			CountryCode{"GreatBritain", "Great Britain and Wales"},
+			CountryCode{"IrelandNorthern", "Northern Ireland"},
+			CountryCode{"IrelandRepublicOf", "Republic of Ireland"},
+			CountryCode{"Scotland", "Scotland"},
+			CountryCode{"UnitedStates", "United States"},
 		},
 	}
 	xml.Unmarshal(xmlFile, &actual)
-	for index, actualCountryCode := range actual.Body.GetCountriesAvailableResponse.
-		GetCountriesAvailableResult.
-		CountryCode {
-		expectedCountryCode := expected.Body.GetCountriesAvailableResponse.GetCountriesAvailableResult.CountryCode[index]
+	for index, actualCountryCode := range actual.CountryCode {
+		expectedCountryCode := expected.CountryCode[index]
 		if expectedCountryCode != actualCountryCode {
 			t.Errorf("expected at index: %d %s but it got %s", index, expectedCountryCode, actualCountryCode)
 		}
